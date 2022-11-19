8a879170d3186e5550e6266d3f40820a9ed5c58f
Dhruv agarwal <dhruv@Dhruvs-MacBook-Pro.local>
2022-11-20 00:00:47 +0530

    Proof of publish NFT completed

--- Contracts/POPNFT.sol
+++ Contracts/POPNFT.sol
@@ -1,30 +1,101 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@thirdweb-dev/contracts/base/ERC721Base.sol";
-import "@thirdweb-dev/contracts/extension/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract POPNFT is ERC721Base {
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        address _royaltyRecipient,
-        uint128 _royaltyBps
-    ) ERC721Base(_name, _symbol, _royaltyRecipient, _royaltyBps) {}
+/// SOUL BOUND Token minted directly to the
+contract POPNFT is ERC721, ERC721URIStorage, Ownable {
+    address public managerAddress;
 
-    /// use mintTo(_to , _uri ) function from the contract to enter the customized URI for the respective token Mint
-    function mintTo(address _to, string memory _tokenURI)
-        public
-        virtual
-        override
-    {
-        _setTokenURI(nextTokenIdToMint(), _tokenURI);
-        _safeMint(_to, 1, "");
+    constructor() ERC721("ProofOfPublish", "POP") {}
+
+    function setManager(address _newAddress) public onlyOwner {
+        require(_newAddress != address(0), "Not a valid address");
+        managerAddress = _newAddress;
     }
 
-    // Function to receive Ether. msg.data must be empty
-    receive() external payable {}
+    modifier onlyManager() {
+        require(msg.sender == managerAddress, "Not Authorized");
+        _;
+    }
 
-    // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public onlyManager {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        require(
+            to == address(0) || from == address(0),
+            "The NFT is non transferrable"
+        );
+        // super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    /// can be called by the owner of token to exit the DAO
+    /// Burns the token ID from the users Account
+    function burn(uint256 tokenId) external {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "Only owner of the token can burn it"
+        );
+        _burn(tokenId);
+    }
+
+    /// function to remove someone from the DAO  , called only by the owner
+    /// will burn the token ID from the users account
+    function revoke(uint256 tokenId) external onlyOwner {
+        _burn(tokenId);
+    }
+
+    /// after any token transfer , events are emitted, revoke show when the NFT is burnt
+    /// attest when NFT is minted
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        if (from == address(0)) {
+            emit Attest(to, tokenId);
+        } else if (to == address(0)) {
+            emit Revoke(to, tokenId);
+        }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }

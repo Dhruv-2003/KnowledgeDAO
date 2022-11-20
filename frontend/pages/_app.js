268e1cbb6fbd148f2a331a8e7048424f00610f23
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-11-20 13:48:43 +0530

    publish page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -12,50 +12,53 @@ import {
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
-const wallabyChain: Chain = {
-  id: 31415,
-  name: "Filecoin — Wallaby testnet",
-  network: "Filecoin — Wallaby testnet",
-  nativeCurrency: {
-    decimals: 18,
-    name: "Test Filecoin",
-    symbol: "tFIL",
-  },
-  rpcUrls: {
-    default: "https://wallaby.node.glif.io/rpc/v0",
-  },
-  blockExplorers: {
-    default: {
-      name: "Wallaby Explorer",
-      url: "https://explorer.glif.io/wallaby",
-    },
-  },
-  testnet: true,
-};
+// const wallabyChain: Chain = {
+//   id: 31415,
+//   name: "Filecoin — Wallaby testnet",
+//   network: "Filecoin — Wallaby testnet",
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: "Test Filecoin",
+//     symbol: "tFIL",
+//   },
+//   rpcUrls: {
+//     default: "https://wallaby.node.glif.io/rpc/v0",
+//   },
+//   blockExplorers: {
+//     default: {
+//       name: "Wallaby Explorer",
+//       url: "https://explorer.glif.io/wallaby",
+//     },
+//   },
+//   testnet: true,
+// };
 
-const { chains, provider } = configureChains(
-  [wallabyChain],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
-);
+// const { chains, provider } = configureChains(
+//   [wallabyChain],
+//   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+// );
 
-const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  chains,
-});
+// const { connectors } = getDefaultWallets({
+//   appName: "My RainbowKit App",
+//   chains,
+// });
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
+// const wagmiClient = createClient({
+//   autoConnect: true,
+//   connectors,
+//   provider,
+// });
 
 function MyApp({ Component, pageProps }) {
-  return;
-  <WagmiConfig client={wagmiClient}>
-    <RainbowKitProvider chains={chains}>
-      <Component {...pageProps} />;
-    </RainbowKitProvider>
-  </WagmiConfig>;
+  return <Component {...pageProps} />
 }
+// function MyApp({ Component, pageProps }) {
+//   return(
+//   // <WagmiConfig client={wagmiClient}>
+//     {/* <RainbowKitProvider chains={chains}> */}
+//       <Component {...pageProps} />
+//     {/* </RainbowKitProvider> */}
+//   {/* </WagmiConfig>); */}
+// }
 
 export default MyApp;

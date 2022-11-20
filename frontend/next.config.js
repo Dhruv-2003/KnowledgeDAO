d664806abb6b02a0ec9333757bbfa4d214a0a652
Dhruv agarwal <dhruv@Dhruvs-MacBook-Pro.local>
2022-11-20 23:05:41 +0530

    deploy changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,23 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    // here you can add the url's that you are planning
-    // to use inside your next/image.
-    domains: ["ipfs.io"],
-  },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-      "/p/hello-nextjs": { page: "/post", query: { title: "hello-nextjs" } },
-      "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } },
-      "/p/deploy-nextjs": { page: "/post", query: { title: "deploy-nextjs" } },
-    };
-  },
 };
 
 module.exports = nextConfig;

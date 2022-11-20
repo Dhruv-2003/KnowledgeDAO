9360db5f4855da4af18adf0fbcf42d046566b769
Dhruv agarwal <dhruv@Dhruvs-MacBook-Pro.local>
2022-11-20 18:15:30 +0530

    Article Publish added

--- frontend/functionality/storeMetadata.js
+++ frontend/functionality/storeMetadata.js
@@ -4,13 +4,19 @@ import { NFTStorage } from "nft.storage";
 import { NFT_STORAGE_API_KEY } from "../constants";
 
 /// used NFT.storage to prepare the metadata for the NFT
-export const StoreMetadata = async (image, Name, invoiceURI) => {
+export const StoreMetadata = async (
+  image,
+  Name,
+  PublisherName,
+  articleURI,
+  articleLink
+) => {
   console.log("Preparing Metadata ....");
   const nft = {
     image: image,
     name: Name,
-    description: `The Bill file is stored here : ${audioCID} .Check more details on the website`,
-    external_url: invoiceURI,
+    description: `The Article was Published by ${PublisherName} . The content of the article is stored on IPFS here : ${articleURI} . It can be read here : ${articleLink}`,
+    external_url: articleLink,
   };
   console.log("Uploading Metadata to IPFS ....");
   const client = new NFTStorage({ token: NFT_STORAGE_API_KEY });
